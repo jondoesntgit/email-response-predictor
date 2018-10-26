3059203b53269ea8b8af64b57c939e8dbbaef473
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2018-10-25 17:13:35 -0700

    Updated the email downloader. It now accurately places 200 of the most recent emails in a sqlite3 file

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,6 +6,7 @@ import imaplib
 import email
 import sqlite3
 from pathlib import Path
+from tqdm import tqdm
 
 OUTPUT_DIRECTORY = Path('../../data/raw')
 SQLITE3_PATH = OUTPUT_DIRECTORY / 'data.sqlite3'
@@ -21,7 +22,8 @@ SMTP_SERVER = "imap.gmail.com"
 SMTP_PORT   = 993
 
 EMAIL_FOLDERS = {
-    'received' : 'inbox',
+    #'received' : 'inbox',
+    'received' : '[Gmail]/Archive',
     'sent' : '"[Gmail]/Sent Mail"'
 }
 
@@ -41,10 +43,11 @@ def download_emails_from_gmail():
 
         id_list = mail_ids.split()
 
-        for file, i in enumerate(id_list[::-1]):
+        num2download = 200
+        for file, i in tqdm(enumerate(id_list[-num2download:]), total=num2download, desc=folder_name):
             typ, data = mail.fetch(i, '(RFC822)' )
             for response_part in data:
-                with open(f'{save_to_directory}/{file}.eml', 'wb') as f:
+                with open(f'{save_to_directory}/{file:06d}.eml', 'wb') as f:
                     f.write(data[0][1])
 
 def index_emails():
@@ -52,14 +55,25 @@ def index_emails():
     c = conn.cursor()
     c.execute('''DROP TABLE IF EXISTS emails;''')
     c.execute('''CREATE TABLE emails
-        (filename text, folder text, sender text, subject text, body text, bodytype text, 
-        message_id text, reply_id text, is_reply bool);''')
+        (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        filename text, 
+        folder text, 
+        sender text, 
+        subject text, 
+        body text, 
+        bodytype text, 
+        message_id text, 
+        reply_id text, 
+        did_reply bool);''')
 
     for folder in EMAIL_FOLDERS:
         folder_path = Path(OUTPUT_DIRECTORY) / folder
         for email_file in folder_path.glob('**/*.eml'):
             with open(email_file) as f:
-                m = email.message_from_file(f)
+                try:
+                    m = email.message_from_file(f)
+                except:
+                    continue
 
             body = ''
             body_type = 'html'
@@ -76,10 +90,28 @@ def index_emails():
 
             fields = (str(email_file.name), str(email_file.parent.name), 
                 m['from'], m['subject'], body, body_type,
-                m['Message-Id'], reply_id, bool(reply_id))
-            c.execute('INSERT INTO emails VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', fields)
+                m['Message-Id'], reply_id, 0)
+            c.execute('''INSERT INTO emails (filename, folder, sender, subject, body, bodytype, message_id, reply_id, did_reply)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', fields)
+
+    conn.commit()
+
+def flag_emails_with_responses():
+    conn = sqlite3.connect(str(SQLITE3_PATH))
+    c = conn.cursor()
+    c.execute('''
+    UPDATE emails 
+    SET did_reply = 1 
+    WHERE id in (
+        SELECT e_received.id 
+        FROM emails as e_received 
+        INNER JOIN emails as e_sent 
+        WHERE e_sent.reply_id = e_received.message_id
+        );
+    ''')
     conn.commit()
 
 if __name__ == "__main__":
     download_emails_from_gmail()
     index_emails()
+    flag_emails_with_responses()
